d5bea5061461b9ed07dc53d76fefb68c25f13517
Alessandro Tazza <atazza@student.ethz.ch>
2025-10-14 17:57:09 +0200

    - Added european option - Added tests - Fixed american option test

--- tests/test_binomial_crr_american_vanilla_option_cpu.cpp
+++ tests/test_binomial_crr_american_vanilla_option_cpu.cpp
@@ -4,7 +4,7 @@
 #include "binomial_crr_american_vanilla_option_cpu.hpp"
 #include <cmath>
 
-TEST_CASE("American Option Put-Call Parity Approximation", "[put_call_parity]") {
+TEST_CASE("American Option Put-Call Parity Approximation", "[ame_put_call_parity]") {
     double S = 100.0;
     double K = 100.0;
     double T = 1.0;
@@ -13,8 +13,8 @@ TEST_CASE("American Option Put-Call Parity Approximation", "[put_call_parity]") 
     double q = 0.0; // no dividends
     int n = 500;
 
-    double putPrice = binomial_crr_american_vanilla_option_cpu(T, S, K, r, sigma, q, n, OptionType::Put);
-    double callPrice = binomial_crr_american_vanilla_option_cpu(T, S, K, r, sigma, q, n, OptionType::Call);
+    double putPrice = binomial_crr_american_vanilla_option_cpu(S, K, T, r, sigma, q, n, OptionType::Put);
+    double callPrice = binomial_crr_american_vanilla_option_cpu(S, K, T, r, sigma, q, n, OptionType::Call);
 
     double callPriceParity = putPrice + S - K * std::exp(-r * T);
 
@@ -23,4 +23,23 @@ TEST_CASE("American Option Put-Call Parity Approximation", "[put_call_parity]") 
     INFO("Call Price from Parity: " << callPriceParity);
 
     REQUIRE(std::abs(callPrice - callPriceParity) < 1.0);
+}
+
+TEST_CASE("American Option Linear Homogeneity", "[ame_linear_hom]") {
+    double S = 100.0;
+    double K = 150.0;
+    double T = 1.0;
+    double r = 0.05;
+    double sigma = 0.2;
+    double q = 0.01; 
+    int n = 500;
+    double alpha = 1.92767;
+
+    double callPrice = binomial_crr_american_vanilla_option_cpu(S, K, T, r, sigma, q, n, OptionType::Call);
+    double callPriceS = binomial_crr_american_vanilla_option_cpu(alpha*S, alpha*K, T, r, sigma, q, n, OptionType::Call);
+
+    INFO("Call + Scaling: " << alpha * callPrice);
+    INFO("Call with Scaled S,K: " << callPriceS);
+
+    REQUIRE(std::abs(alpha*callPrice - callPriceS) < 1.0e-6);
 }
