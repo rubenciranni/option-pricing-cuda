1f4595b8bebaf8264d2a4994f3964a7f638b4403
rubenciranni <rubenciranni6@gmail.com>
2025-10-16 14:56:03 +0200

    fix docstring binomial eur

--- include/backends/cpu/binomial_crr_european_vanilla_option_cpu.hpp
+++ include/backends/cpu/binomial_crr_european_vanilla_option_cpu.hpp
@@ -3,7 +3,7 @@
 #include "constants.hpp"
 
 /**
- * @brief Prices an American option using the Cox–Ross–Rubinstein (CRR) binomial model.
+ * @brief Prices an European option using the Cox–Ross–Rubinstein (CRR) binomial model.
  *
  * @param S      Current underlying asset price.
  * @param K      Strike price of the option.
@@ -12,7 +12,7 @@
  * @param sigma  Volatility of the underlying asset (annualized).
  * @param q      Continuous dividend yield.
  * @param n      Number of binomial steps.
- * @return The computed American option price.
+ * @return The computed European option price.
  */
 double binomial_crr_european_vanilla_option_cpu(const double S, const double K, const double T,
                                                 const double r, const double sigma, const double q,
