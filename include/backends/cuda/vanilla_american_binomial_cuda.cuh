92944aad9327499eaa1395e589a8117d06447cf3
rubenciranni <rubenciranni6@gmail.com>
2025-10-25 13:46:48 +0200

    refactor

--- include/backends/cuda/vanilla_american_binomial_cuda.cuh
+++ include/backends/cuda/vanilla_american_binomial_cuda.cuh
@@ -7,5 +7,5 @@ double vanilla_american_binomial_cuda(const double S, const double K, const doub
                                       const int n, const OptionType type);
 
 double vanilla_american_binomial_cuda_no_sync(const double S, const double K, const double T,
-                                      const double r, const double sigma, const double q,
-                                      const int n, const OptionType type);
+                                              const double r, const double sigma, const double q,
+                                              const int n, const OptionType type);
