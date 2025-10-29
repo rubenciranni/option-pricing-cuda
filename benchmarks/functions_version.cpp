3d20ae6ffbbed25f5e40b44419299cf56fbacb87
Nicolas Dickenmann <ndickenmann@studgpu-node01.inf.ethz.ch>
2025-10-29 16:13:42 +0100

    feat: precompute pow on cpu

--- benchmarks/functions_version.cpp
+++ benchmarks/functions_version.cpp
@@ -14,4 +14,5 @@ std::map<std::string, PricingFunction> FUNCTIONS = {
      vanilla_american_binomial_cpu_index_opt_cache},
     {"vanilla_american_binomial_cpu_remove_zeros", vanilla_american_binomial_cpu_remove_zeros},
     {"vanilla_american_binomial_cuda_naive", vanilla_american_binomial_cuda_naive},
-    {"vanilla_american_binomial_cuda_no_sync", vanilla_american_binomial_cuda_no_sync}};
+    {"vanilla_american_binomial_cuda_no_sync", vanilla_american_binomial_cuda_no_sync},
+    {"vanilla_american_binomial_cuda_precomputed", vanilla_american_binomial_cuda_precomputed}};
