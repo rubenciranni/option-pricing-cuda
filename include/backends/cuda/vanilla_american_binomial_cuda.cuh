5ad25c4e29ea457c4a2ceb083692194473ef5b99
Giovanni Spadaccini <giospadaccini74@gmail.com>
2025-10-30 15:34:06 +0100

    Remove unused function declaration for unroll_tile
    Removed unused function declaration for vanilla_american_binomial_cuda_unroll_tile.

--- include/backends/cuda/vanilla_american_binomial_cuda.cuh
+++ include/backends/cuda/vanilla_american_binomial_cuda.cuh
@@ -22,11 +22,6 @@ double vanilla_american_binomial_cuda_unroll(const double S, const double K, con
                                              const double r, const double sigma, const double q,
                                              const int n, const OptionType type);
 
-double vanilla_american_binomial_cuda_unroll_tile(const double S, const double K, const double T,
-                                                  const double r, const double sigma,
-                                                  const double q, const int n,
-                                                  const OptionType type);
-
 inline double vanilla_american_binomial_cuda(const double S, const double K, const double T,
                                              const double r, const double sigma, const double q,
                                              const int n, const OptionType type) {
