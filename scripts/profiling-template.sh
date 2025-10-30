523d1efb7bdf494475f8429e09f634539960babf
Giovanni Spadaccini <gspadaccini@student-cluster2.inf.ethz.ch>
2025-10-30 20:32:33 +0100

    added script for running the profiler

---
+++ scripts/profiling-template.sh
@@ -0,0 +1,37 @@
+# echo in red
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+echo -e "${RED}WARNING: This is a template script for profiling. Modify it as needed.${NC}"
+
+# PARAMETERS
+
+# default parameter hard dataset
+REGEX_KERNEL="${1}"
+PARAMETER="${2:-cuda_debug}"
+echo "Profiling with parameters set: ${PARAMETER}"
+echo "And kernel regex: ${REGEX_KERNEL}"
+
+## BUILD
+
+cd ./build || exit 1
+bash ../scripts/compile_on_cluster.sh || exit 1
+make -j 4 || exit 1
+
+## PROFILE
+
+
+# ---- Nsight Compute
+
+srun --pty -A dphpc -t 60 \
+  nsys profile -o ../profile_res/profile_report \
+ ./bin/pricing_cli benchmark --parameters $PARAMETER   --filter-by-name cuda --no-verify
+  # here we run only the cuda implementation benchmarks 
+
+# ----- Nsight Systems
+
+# RUn sbatch system
+srun --pty -A dphpc -t 60 \
+  ncu --kernel-name $REGEX_KERNEL \
+    --launch-count 1  -o ../profile_res/profile_kernel \
+ ./bin/pricing_cli benchmark --parameters $PARAMETER   --filter-by-name cuda  --no-verify
+  # here we run only the cuda implementation benchmarks 
