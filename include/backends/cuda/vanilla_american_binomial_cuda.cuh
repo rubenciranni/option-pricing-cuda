af9f09a3d7abffc5015e54d6b31f761c60456958
giospada <giospadaccini74@gmail.com>
2025-11-01 00:32:50 +0100

    sorry ruben i don´t want to name this commit

--- include/backends/cuda/vanilla_american_binomial_cuda.cuh
+++ include/backends/cuda/vanilla_american_binomial_cuda.cuh
@@ -41,6 +41,7 @@ double vanilla_american_binomial_cuda_x_y_unroll(const double S, const double K,
                                                  const double r, const double sigma, const double q,
                                                  const int n, const OptionType type);
 
+
 inline double vanilla_american_binomial_cuda(const double S, const double K, const double T,
                                              const double r, const double sigma, const double q,
                                              const int n, const OptionType type) {
