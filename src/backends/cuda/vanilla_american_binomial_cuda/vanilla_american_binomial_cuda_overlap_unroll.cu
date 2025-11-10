a39ba0af8849a353203dfcab3232041a2cf1a940
Ruben Ciranni <rubenciranni6@gmail.com>
2025-11-10 11:37:29 +0100

    overlap_unroll: minor optimizations

--- src/backends/cuda/vanilla_american_binomial_cuda/vanilla_american_binomial_cuda_overlap_unroll.cu
+++ src/backends/cuda/vanilla_american_binomial_cuda/vanilla_american_binomial_cuda_overlap_unroll.cu
@@ -11,23 +11,27 @@
 #define THREADS_PER_BLOCK 128
 #define UNROLL_FACTOR 32
 
-__global__ void fill_st_buffer_kernel_overlap_unroll(double* __restrict__ st_buffer, const double S,
-                                                     const double K, const double u, const int sign,
-                                                     const int n, const int parity) {
+__global__ void fill_st_buffers_kernel_overlap_unroll(double* __restrict__ st_buffer_bank0,
+                                                      double* __restrict__ st_buffer_bank1,
+                                                      const double S, const double K,
+                                                      const double u, const int sign, const int n) {
     int threadId = blockIdx.x * blockDim.x + threadIdx.x;
     if (threadId >= n + 1) return;
 
-    st_buffer[threadId] = fmax(sign * fma(S, pow(u, (double)2 * threadId - n + parity), -K), 0.0);
+    double u_pow_2_threadId = pow(u, (double)2 * threadId);
+    double u_pow_minus_n = pow(u, (double)-n);
+
+    // entry i stores value corresponding to exponent 2*i - n
+    st_buffer_bank0[threadId] = fmax(sign * fma(S, u_pow_2_threadId * u_pow_minus_n, -K), 0.0);
+
+    // entry i stores value corresponding to exponent 2*i - n + 1
+    st_buffer_bank1[threadId] = fmax(sign * fma(S, u_pow_2_threadId * u_pow_minus_n * u, -K), 0.0);
 }
 
 __global__ void compute_next_layers_kernel_overlap_unroll(
     double* layer_values_read, double* layer_values_write, double* st_buffer_bank0,
     double* st_buffer_bank1, const double up, const double down, const int level, const int n) {
-    __shared__ double layer_values_tile_read_array[THREADS_PER_BLOCK + 1];
-    __shared__ double layer_values_tile_write_array[THREADS_PER_BLOCK + 1];
-
-    double* layer_values_tile_read = layer_values_tile_read_array;
-    double* layer_values_tile_write = layer_values_tile_write_array;
+    __shared__ double layer_values_tile[2][THREADS_PER_BLOCK + 1];
 
     int tile_stride = THREADS_PER_BLOCK - UNROLL_FACTOR;
     int tile_base = tile_stride * blockIdx.x;
@@ -35,47 +39,45 @@ __global__ void compute_next_layers_kernel_overlap_unroll(
 
     if (node_id >= level + UNROLL_FACTOR) return;
 
-    layer_values_tile_read[threadIdx.x] = layer_values_read[node_id];
+    layer_values_tile[0][threadIdx.x] = layer_values_read[node_id];
 
     __syncthreads();
 
 #pragma unroll
-    for (int i = UNROLL_FACTOR - 1; i >= 0; i--) {
-        int current_level = level + i;
+    for (int i = 0; i < UNROLL_FACTOR; i++) {
+        int read_idx = i % 2;
+        int write_idx = (i + 1) % 2;
+
+        int current_level = level + UNROLL_FACTOR - 1 - i;
         double* st_buffer_bank = (n - current_level) % 2 ? st_buffer_bank1 : st_buffer_bank0;
 
-        double hold = fma(up, layer_values_tile_read[threadIdx.x + 1],
-                          down * layer_values_tile_read[threadIdx.x]);
+        double hold = fma(up, layer_values_tile[read_idx][threadIdx.x + 1],
+                          down * layer_values_tile[read_idx][threadIdx.x]);
         int st_index = node_id + (n - current_level) / 2;
         double exercise = st_buffer_bank[st_index];
-        layer_values_tile_write[threadIdx.x] = fmax(hold, exercise);
+        layer_values_tile[write_idx][threadIdx.x] = fmax(hold, exercise);
 
         __syncthreads();
-
-        double* tmp = layer_values_tile_write;
-        layer_values_tile_write = layer_values_tile_read;
-        layer_values_tile_read = tmp;
     }
 
     if (node_id <= n && threadIdx.x < THREADS_PER_BLOCK - UNROLL_FACTOR) {
-        layer_values_write[node_id] = layer_values_tile_read[threadIdx.x];
+        layer_values_write[node_id] = layer_values_tile[UNROLL_FACTOR % 2][threadIdx.x];
     }
 }
 
-__global__ void compute_next_layer_kernel_overlap_unroll(
-    double* layer_values_read, double* layer_values_write, double* st_buffer_bank0,
-    double* st_buffer_bank1, const double up, const double down, const int level, const int n) {
-    int threadId = blockIdx.x * blockDim.x + threadIdx.x;
-    if (threadId >= level + 1) return;
-
-    /*
+/*
     At each layer l exercise value of node i (from the bottom) is calculated with the following
     exponent: 2*i - l = 2 * (i + (n - l) / 2) - n           if (n - l) even
     the correspoding value is stored at st_buffer_bank0[i + (n - l) / 2]
 
     2*i - l = 2 * (i + (n - l - 1) / 2) - n + 1   if (n - l) odd
     the correspoding value is stored at st_buffer_bank1[i + (n - l) / 2]
-    */
+*/
+__global__ void compute_next_layer_kernel_overlap_unroll(
+    double* layer_values_read, double* layer_values_write, double* st_buffer_bank0,
+    double* st_buffer_bank1, const double up, const double down, const int level, const int n) {
+    int threadId = blockIdx.x * blockDim.x + threadIdx.x;
+    if (threadId >= level + 1) return;
 
     double hold = up * layer_values_read[threadId + 1] + down * layer_values_read[threadId];
     double* st_buffer_bank = (n - level) % 2 ? st_buffer_bank1 : st_buffer_bank0;
@@ -105,10 +107,8 @@ double vanilla_american_binomial_cuda_overlap_unroll(const double S, const doubl
     cudaMalloc(&st_buffer_bank1_d, (n + 1) * sizeof(double));
 
     int num_blocks = std::ceil((n + 1) * 1.0 / THREADS_PER_BLOCK);
-    fill_st_buffer_kernel_overlap_unroll<<<num_blocks, THREADS_PER_BLOCK>>>(st_buffer_bank0_d, S, K,
-                                                                            u, sign, n, 0);
-    fill_st_buffer_kernel_overlap_unroll<<<num_blocks, THREADS_PER_BLOCK>>>(st_buffer_bank1_d, S, K,
-                                                                            u, sign, n, 1);
+    fill_st_buffers_kernel_overlap_unroll<<<num_blocks, THREADS_PER_BLOCK>>>(
+        st_buffer_bank0_d, st_buffer_bank1_d, S, K, u, sign, n);
 
     // Layer n is the first n + 1 entries of st_buffer_bank0_d
     cudaMemcpy(layer_values_read_d, st_buffer_bank0_d, (n + 1) * sizeof(double),
