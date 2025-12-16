f5b390e235a4316322ed7c434f694dea59f5fdca
rubenciranni <rubenciranni6@gmail.com>
2025-12-16 15:02:04 +0100

    Revert "add some visualization utils"
    This reverts commit dcdeb1f86e0fe1e03830e87721f73b3b3b58c528.

--- sbatch.sh
+++ sbatch.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-#SBATCH --time=02:00:00
+#SBATCH --time=00:10
 #SBATCH --account=dphpc
-#SBATCH --output=128-NStepCM2-111.json
+#SBATCH --output=benchmark.json
 
-# ./build/bin/pricing_cli benchmark --filter-by-name vanilla_american_binomial_cuda_bkdstprcmp_xdovlpunroll_vtile_trimotm --parameters large-reprare --reference-function vanilla_american_binomial_cuda_naive --output-format=json
-./build/bin/pricing_cli benchmark --filter-by-name shuffle --parameters xl-crazy --reference-function vanilla_american_binomial_cuda_bkdstprcmp_xdovlpunroll_vtile_trimotm --output-format=json
+./build/bin/pricing_cli benchmark --filter-by-name vanilla_american_binomial_cuda_bkdstprcmp_xdovlpunroll_vtile_trimotm --parameters large-reprare --reference-function vanilla_american_binomial_cuda_naive --output-format=json
